a296657fc8fe24fc37bc9d025e74bc8061cf3ae8
brinereef1 <santra.brine2020@gmail.com>
2023-06-26 08:31:03 +0530

    age calculator app solution

---
+++ script.js
@@ -0,0 +1,12 @@
+const day = document.getElementById('day')
+const month = document.getElementById('month')
+const year = document.getElementById('year');
+
+const calculateAge = () => {
+
+  const resultDay = ''
+  console.log(resultDay)
+  day.value = ''
+  month.value = ''
+  year.value = ""
+}
